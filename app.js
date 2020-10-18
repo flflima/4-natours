243ee9bbe897208b5adfdc984e4b1c01081613fb
Felipe Lima <flf.felipe@gmail.com>
2020-10-18 00:57:32 -0300

    Implementa POST na api

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 // app.get('/', (req, res) => {
 //     res.status(200).json({
 //         message: 'Hello from the server side!',
@@ -14,7 +16,7 @@ const app = express();
 //     res.send('You can post from this endpoint!!');
 // });
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours.json`));
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
 app.get('/api/v1/tours', (req, res) => {
     res.status(200).json({
@@ -23,6 +25,24 @@ app.get('/api/v1/tours', (req, res) => {
         data: {
             tours
         }
+    });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+    // console.log(req.body);
+
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({id: newId}, req.body);
+
+    tours.push(newTour);
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        });
     });
 });
 
