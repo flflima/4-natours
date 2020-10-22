95a24f24bcd45d20bdd50d713c8cc5514bd1f0c5
Felipe Lima Freire <felipe.lima.freire@concrete.com.br>
2020-10-22 19:19:14 -0300

    Adicionando eslint

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
 const dotenv = require('dotenv');
 
-console.log(process.env.NODE_ENV);
-
-dotenv.config({ path: './config.env'});
+dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
-console.log(process.env);
-
 const port = process.env.PORT;
 app.listen(port, () => {
-    console.log(`App running on port ${port}`);
+  console.log(`App running on port ${port}`);
 });
