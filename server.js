b647075167663572d7605050ccd2820d9dbd26b7
Felipe Lima Freire <felipe.lima.freire@concrete.com.br>
2020-10-22 12:55:19 -0300

    Usando variaveis de ambiente

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
+const dotenv = require('dotenv');
+
+console.log(process.env.NODE_ENV);
+
+dotenv.config({ path: './config.env'});
+
 const app = require('./app');
 
-const port = 3000;
+console.log(process.env);
+
+const port = process.env.PORT;
 app.listen(port, () => {
     console.log(`App running on port ${port}`);
 });
