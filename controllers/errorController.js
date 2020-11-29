be0738a5e1fa9bd9d676a7c447573389e8e41459
Felipe Lima <flf.felipe@gmail.com>
2020-11-28 23:56:02 -0300

    Erros em deve e em prod

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,11 +1,35 @@
-module.exports = (err, req, res, next) => {
-  console.log(err.stack);
+const sendErrorDev = (res, err) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
 
+const sendErrorProd = (res, err) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.error('Error ', err);
+    
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!',
+    });
+  }
+};
+
+module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(res, err);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(res, err);
+  }
 };
