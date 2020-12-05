ac70eaa740e199aff263e57825aa1a7dfa597c93
Felipe Lima <flf.felipe@gmail.com>
2020-12-05 13:46:04 -0300

    Tratando Unhandled Rejections

--- server.js
+++ server.js
@@ -14,6 +14,14 @@ mongoose.connect(DB, {
 }).then(() => console.log('DB connection successful'));
 
 const port = process.env.PORT;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLER REJECTION! Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
