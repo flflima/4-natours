2b16c445fbdb703409345419e40507531401fc38
Felipe Lima <flf.felipe@gmail.com>
2021-01-02 18:40:46 -0300

    Implementando ROLES e PERMISSIONS

--- controllers/authController.js
+++ controllers/authController.js
@@ -24,6 +24,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
     passwordChangedAt: req.body.passwordChangedAt,
+    role: req.body.role,
   });
 
   const token = signToken(newUser._id);
@@ -105,3 +106,13 @@ exports.protect = catchAsync(async (req, res, next) => {
 
   next();
 });
+
+exports.restrictTo = (...roles) => (req, res, next) => {
+  if (!roles.includes(req.user.role)) {
+    return next(
+      new AppError('You do not have permission to perform this action', 403)
+    );
+  }
+
+  next();
+};
