150fb14b7a0cf8735ed44cc8c74029775d95f218
Felipe Lima <flf.felipe@gmail.com>
2021-01-04 18:17:32 -0300

    Query middleware para filtrar apenas ativos

--- models/userModel.js
+++ models/userModel.js
@@ -63,6 +63,15 @@ userSchema.pre('save', async function (next) {
   next();
 });
 
+userSchema.pre(/^find/, function (next) {
+  this.find({
+    active: {
+      $ne: false
+    }
+  });
+  next();
+});
+
 userSchema.methods.correctPassword = async function (
   candidatePassword,
   userPassword
