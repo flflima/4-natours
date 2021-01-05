cab4b338ff1058980271bc3c78fe7b9002f70b47
Felipe Lima <flf.felipe@gmail.com>
2021-01-05 16:36:53 -0300

    Sanitiza os dados recebidos para evitar ataques externos

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const morgan = require('morgan');
-// const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -8,6 +7,8 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const app = express();
 
@@ -32,6 +33,11 @@ app.use('/api', limiter);
 app.use(express.json({
   limit: '10kb'
 }));
+
+// data sanitization
+app.use(mongoSanitize());
+
+app.use(xss()); 
 
 app.use(express.static(`${__dirname}/public`));
 
