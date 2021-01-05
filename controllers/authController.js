4fef36f21651ded9aaddf1471a67ff56eb205b00
Felipe Lima <flf.felipe@gmail.com>
2021-01-05 15:54:15 -0300

    Enviando token JWT no cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -25,6 +25,17 @@ const signToken = (id) => {
 const createAndSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
 
+  const cookieOptions = {
+    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') {
+    cookieOptions.secure = true;
+  }
+
+  res.cookie('jwt', cookieOptions);
+
   // remove passsword from body response on signup
   user.password = undefined;
 
