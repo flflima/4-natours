72ae902655e5dcd5d214e4124de3b524ba3d8409
Felipe Lima <flf.felipe@gmail.com>
2021-01-10 19:29:45 -0300

    Populando virtual fields

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -34,11 +34,16 @@ const reviewSchema = new mongoose.Schema({
 });
 
 reviewSchema.pre(/^find/, function (next) {
+    // this.populate({
+    //     path: 'user',
+    //     select: 'name'
+    // }).populate({
+    //     path: 'tour',
+    //     select: 'name'
+    // });
+
     this.populate({
         path: 'user',
-        select: 'name'
-    }).populate({
-        path: 'tour',
         select: 'name'
     });
 
