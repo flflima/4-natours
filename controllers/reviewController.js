17974da44d66ed8e69e2fd411c365bd63265eff4
Felipe Lima <flf.felipe@gmail.com>
2021-01-10 21:45:08 -0300

    Implementando nested routes

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -2,6 +2,9 @@ const Review = require("../models/reviewModel");
 const catchAsync = require("../utils/catchAsync");
 
 exports.createReview = catchAsync(async (req, res, next) => {
+    if (!req.body.tour) req.body.tour = req.params.tourId;
+    if (!req.body.user) req.body.user = req.user.id;
+
     const review = await Review.create(req.body);
 
     res.status(201).json({
